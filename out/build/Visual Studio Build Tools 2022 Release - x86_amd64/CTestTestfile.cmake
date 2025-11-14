0704206067a6d5ec0002bbcd9a422c9a86d79dd4
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-11-14 11:48:05 -0800

    got it compiling

---
+++ out/build/Visual Studio Build Tools 2022 Release - x86_amd64/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/their/Documents/GitHub/robots
+# Build directory: C:/Users/their/Documents/GitHub/robots/out/build/Visual Studio Build Tools 2022 Release - x86_amd64
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
